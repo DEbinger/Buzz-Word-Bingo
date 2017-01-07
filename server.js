101edfbb2cbd6c81cd40df2db98ccb5ef57b8a12
David <davidebinger888@gmail.com>
2017-01-06 14:05:49 -1000

    adding PUT, not sure if working as expected

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/', ( req, res, next ) => {
   });
 
 app.get('/buzzwords', ( req, res, next ) => {
-  res.send(buzzwordArr);
+  res.send(buzzWords);
 });
 
 app.post('/buzzword', ( req, res, next ) => {
@@ -26,9 +26,18 @@ app.post('/buzzword', ( req, res, next ) => {
   res.send( '{ "success": true } ');
 });
 
+app.put('/buzzword', (req, res, next)=> {
+  for(let i = 0; i < buzzWords.length; i++){
+    if (req.body.buzzWord === buzzWords[i].buzzWord){
+      let pointsValue = parseInt(buzzWords[i].points);
+      let reqNewPoints = parseInt(req.body.points);
+      buzzWords[i].points = pointsValue + reqNewPoints;
+      buzzWords[i].heard = true;
+      res.send({'success': true,'newScore': buzzWords[i].points});
+    }
+  }
+});
+
 app.listen(3000, function(server,  port) {
   console.log("let's get this party started");
 });
-
-//res.sendFile(__dirname + '/index.html')
-//find out what this is
